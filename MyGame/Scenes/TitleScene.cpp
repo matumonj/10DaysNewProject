0f27485e2a9be305e7c19fef1dc47c6a6096cafc
koyamakaito <k020g1197@g.neec.ac.jp>
2022-09-09 01:57:16 +0900

    整理整頓しました

---
+++ MyGame/Scenes/TitleScene.cpp
@@ -0,0 +1,92 @@
+#include "TitleScene.h"
+#include"Input.h"
+#include"SceneManager.h"
+#include"Tutorial.h"
+#include"Feed.h"
+#include"Tuna.h"
+#include"Egg.h"
+#include"CameraControl.h"
+#include"Player.h"
+
+TitleScene::TitleScene(SceneManager* sceneManager)
+	:BaseScene(sceneManager)
+{
+
+}
+/// <summary>
+/// 初期化
+/// </summary>
+void TitleScene::Initialize()
+{
+	CameraControl::GetInstance()->Initialize(CameraControl::GetInstance()->GetCamera());
+	sushis.push_back(new Tuna());
+	for (int i = 0; i < sushis.size(); i++) {
+		sushis[i]->Initialize();
+	}
+	std::unique_ptr<Bench> newBench;
+	Bench* newBench_ = new Bench();
+	newBench.reset(newBench_);
+	Benchs.push_back(std::move(newBench));
+	for (std::unique_ptr<Bench>& bench : Benchs) {
+		bench->Initialize();
+	}
+	Player::GetInstance()->Initialize(CameraControl::GetInstance()->GetCamera());
+	// 3Dオブジェクトにカメラをセット
+	Object3d::SetCamera(CameraControl::GetInstance()->GetCamera());
+
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void TitleScene::Update()
+{ 
+	CameraControl::GetInstance()->Update(CameraControl::GetInstance()->GetCamera());
+	for (int i = 0; i < sushis.size(); i++) {
+		sushis[i]->Update();
+	}
+	for (std::unique_ptr<Bench>& bench : Benchs) {
+		bench->Update();
+	}
+	//Player::GetInstance()->Update(CameraControl::GetInstance()->GetCamera());
+
+	if (Input::GetInstance()->TriggerButton(Input::Button_B)) {//押されたら
+		BaseScene* scene = new Tutorial(sceneManager_);//次のシーンのインスタンス生成
+		SceneManager::GetInstance()->SetScene(SceneManager::TUTORIAL);
+		sceneManager_->SetnextScene(scene);//シーンのセット
+	}
+}
+
+/// <summary>
+/// スプライト描画
+/// </summary>
+/// <param name="cmdList"></param>
+void TitleScene::SpriteDraw()
+{
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+/// <param name="cmdList"></param>
+void TitleScene::Draw()
+{
+	//ポストエフェクトの描画
+	DirectXCommon::GetInstance()->BeginDraw();//描画コマンドの上らへんに
+	SpriteDraw();
+	for (std::unique_ptr<Bench>& bench : Benchs) {
+		bench->Draw();
+	}
+	for (int i = 0; i < sushis.size(); i++) {
+		sushis[i]->Draw();
+	}
+	//やろうとしたがここでエラーを吐く
+	//Player::GetInstance()->Draw();
+	DirectXCommon::GetInstance()->EndDraw();
+
+}
+void TitleScene::Finalize()
+{
+	//delete postEffect;
+	delete titlesprite;
+}
